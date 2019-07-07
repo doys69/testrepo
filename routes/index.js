2d6ed9d259c4cbc82051c443eb482acb8e127102
oktariza <oktariza.akbar@gmail.com>
2019-07-07 15:12:28 +0700

    Add test6

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.get('/test4', function(req, res, next) {
+router.get('/test6', function(req, res, next) {
   res.json({
     test: "OK"
   })
