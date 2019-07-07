e3defb1571cff6f685b75789ffbcf796aaef4399
Pradito Damar Setiady <damarsetiady@gmail.com>
2019-07-07 16:08:07 +0700

    Revert "Create unit test"
    This reverts commit 985543228f2570a8ac12bdc7b0fd385039c63fcf.

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,19 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.get('/test7', function(req, res, next) {
+router.get('/test6', function(req, res, next) {
   res.json({
     test: "OK"
   })
 });
 router.get('/test2', function(req, res, next) {
   res.status(200).json({
-    test: "OK"
+    test2: "OK"
   })
 });
 router.get('/test3', function(req, res, next) {
   res.status(200).json({
-    test: "OK"
+    test3: "OK"
   })
 });
 
