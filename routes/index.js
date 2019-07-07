6282cfb8af822e8e61a8323641ff9e7c83a4c3e6
oktariza <oktariza.akbar@gmail.com>
2019-07-07 14:46:39 +0700

    initial Commit

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/test', function(req, res, next) {
+  res.json({
+    test: "OK"
+  })
+});
+router.get('/test2', function(req, res, next) {
+  res.status(200).json({
+    test2: "OK"
+  })
+});
+
+module.exports = router;
