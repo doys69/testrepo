e3466bcccd1929eb4eddf2214c732ba3d765a707
Pradito Damar Setiady <damarsetiady@gmail.com>
2019-07-07 15:11:52 +0700

    add test7
    add test7

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.get('/test4', function(req, res, next) {
+router.get('/test7', function(req, res, next) {
   res.json({
     test: "OK"
   })
